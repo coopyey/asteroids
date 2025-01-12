95f8709a98f9da880ab2590f012ef28ed4277f1d
Micah Mohr <mjmohr916@gmail.com>
2025-01-12 09:46:53 -0600

    player turning

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
+
+# typically should avoid wildcard imports and only import what you actually need
+# but, alas, small project is small
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -9,6 +14,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # creates gui window
 
+    fpsClock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) # initialize player in center of screen
+
     while 1:
         # checks to see if user used the close button on gui
         # if so, exits the game gracefully
@@ -17,7 +27,12 @@ def main():
                 return
 
         pygame.Surface.fill(screen, (0,0,0)) # set screen to black
+        player.draw(screen) # draw player
+
+        player.update(dt) # takes keyboard input and updates player movement
         pygame.display.flip() # update screen
+        fpsClock.tick(60) # limits the frame rate to 60 fps
+        dt = fpsClock.tick(60) / 1000 # calculates delta time
 
 
 if __name__ == "__main__":
