ee0a1b4d46d23728ce6dcefa6420bee830ba3ac1
coopyey <coopyey@coopyey.gay>
2025-01-11 21:44:47 -0600

    notes

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # creates gui window
 
     while 1:
+        # checks to see if user used the close button on gui
+        # if so, exits the game gracefully
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        pygame.Surface.fill(screen, (0,0,0))
-        pygame.display.flip()
+
+        pygame.Surface.fill(screen, (0,0,0)) # set screen to black
+        pygame.display.flip() # update screen
 
 
 if __name__ == "__main__":
     main()
+
+    
