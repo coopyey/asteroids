280b3fdbfe325ae9b67d4d3dc91df7817c386a8b
coopyey <coopyey@coopyey.gay>
2025-01-13 16:20:10 -0600

    asteroids!

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,11 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, other):
+        dist = pygame.math.Vector2.distance_to(self.position, other.position)
+
+        if dist < (self.radius + other.radius):
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     print("Starting asteroids!")
@@ -12,12 +14,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # creates gui window
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # creates gui window
     fpsClock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers  = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) # initialize player in center of screen
+    field = AsteroidField()
 
     while 1:
         # checks to see if user used the close button on gui
@@ -27,9 +38,18 @@ def main():
                 return
 
         pygame.Surface.fill(screen, (0,0,0)) # set screen to black
-        player.draw(screen) # draw player
 
-        player.update(dt) # takes keyboard input and updates player movement
+        for update_obj in updatable:
+            update_obj.update(dt)
+
+        for ast_obj in asteroids:
+            if ast_obj.collision(player) == True:
+                print("Game over!")
+                return
+
+        for draw_obj in drawable:
+            draw_obj.draw(screen)
+
         pygame.display.flip() # update screen
         fpsClock.tick(60) # limits the frame rate to 60 fps
         dt = fpsClock.tick(60) / 1000 # calculates delta time
